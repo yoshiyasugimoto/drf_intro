78452b76560a734da778026394247eb6f5e7312e
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-09-08 07:07:52 +0900

    Add .env

--- devicelistapi/settings.py
+++ devicelistapi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8(rf_adjqkvn#@5n94lr*0@q3mjs%m826%730#cya8zu!8g+d='
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
